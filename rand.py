be874a088932346e50dca6692a81f18e35e49012
agala2 <ayushgala2@gmail.com>
2025-02-04 22:31:56 -0500

    fixed all static analysis issues. Fixed workflow to run only on pushes to main

--- rand.py
+++ rand.py
@@ -1,8 +1,11 @@
-import subprocess
+"""
+Module for making random number generators
+"""
+import secrets
 
 def random_array(arr):
-    shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    """Function to generate random array without subprocess"""
+
+    for i, _ in enumerate(arr):
+        arr[i] = secrets.randbelow(20) + 1
     return arr

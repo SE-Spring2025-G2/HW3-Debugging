3f6fc224e6bb85febe3c0f37b7473de6ada2bcb4
Keyur Gondhalekar <kbgondh@gmail.com>
2025-02-04 21:33:51 -0500

    Fixed lint and flakes issues

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,45 +1,52 @@
+"""
+Module for the random number functions.
+"""
 import rand
 
-def mergeSort(arr):
-    if (len(arr) == 1):
-        return arr
+def merge_sort(merge_sort_arr):
+    """Function to use merge sort"""
 
-    half = len(arr)//2
+    if len(merge_sort_arr) == 1:
+        return merge_sort_arr
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    half = len(merge_sort_arr)//2
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+    return recombine(merge_sort(merge_sort_arr[:half]), merge_sort(merge_sort_arr[half:]))
+
+def recombine(left_arr, right_arr):
+    """Function to recombine the arrays"""
+
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merge_arr[left_index + right_index] = left_arr[left_index]
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            left_index += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    return mergeArr
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
 
-def BubbleSort(arr):
-    n = len(arr)
-    for indx_1 in range(n):
-        for J in range(0, n-indx_1-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+    return merge_arr
+
+def bubble_sort(bubble_sort_arr):
+    """Function to use bubble sort"""
+
+    n = len(bubble_sort_arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if bubble_sort_arr[j] > bubble_sort_arr[j+1]:
+                bubble_sort_arr[j], bubble_sort_arr[j+1] = bubble_sort_arr[j+1], bubble_sort_arr[j]
+    return bubble_sort_arr
 
 
 arr = rand.random_array([None] * 20)
 arr_out = bubble_sort(arr)
 
 print(arr_out)
-
-
--- rand.py
+++ rand.py
@@ -1,8 +1,13 @@
+"""
+Module for importing subprocesses
+"""
 import subprocess
 
 def random_array(arr):
+    """Function to generate random array"""
+
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    for i, sorted_arr in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
+        sorted_arr[i] = int(shuffled_num.stdout)
     return arr

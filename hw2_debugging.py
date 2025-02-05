484e88cb2136ee0f9aadd7f97c4c0cbbbb66d8d8
Ayush Pathak <ayushpathak@Mac.lan>
2025-02-04 21:37:54 -0500

    added selection sort code

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -28,6 +28,19 @@ def recombine(leftArr, rightArr):
 
     return mergeArr
 
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
 arr = rand.random_array([None] * 20)
 arr_out = mergeSort(arr)
 
